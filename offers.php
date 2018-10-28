dd849f0b39e1b564431945b85b307958166f2bf6
Sajal Garg <sajalgarg063@gmail.com>
2018-10-28 16:33:49 +0530

    init

---
+++ offers.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>About Us | PerfectSmile</title>
+	
+	<!-- core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+
+<?php
+	$page = "offers";
+	include "header.php";
+?>
+    <section id="about-us">
+        <div class="container">
+			<div class="center wow fadeInDown" style="text-align: justify;">
+				<h2 style="text-align: center;font-size:26px">On 10th March 2008, Perfect Smile started a small venture with the aim of imparting ethical and world class dental treatment in Faridabad. On completion of 10 successful years of creating healthy smiles, we, The Perfect smile Team, would like to thank all our patients for continuous faith in us. 
+</h2>
+				<!--<p class="lead" >On 10th March 2008, Perfect Smile started a small venture with the aim of imparting ethical and world class dental treatment in Faridabad. On completion of 10 successful years of creating healthy smiles, we, The Perfect smile Team, would like to thank all our patients for continuous faith in us. 
+<br>-->
+<!--Celebrating the anniversary, Perfect Smile brings out Special Smile Offers for all the patients (VALID ONLY FOR THE MONTH OF MARCH 2018) :-->
+</div>
+			
+			<!-- about us slider -->
+			<div id="about-slider">
+				<div id="carousel-slider" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+				  	<ol class="carousel-indicators visible-xs">
+					    <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-slider" data-slide-to="1"></li>
+					    <li data-target="#carousel-slider" data-slide-to="2"></li>
+				  	</ol>
+
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="assets/offers.jpg" class="img-responsive" alt=""> 
+					   </div>
+					 
+					</div>
+					
+					
+				</div> <!--/#carousel-slider-->
+			</div><!--/#about-slider-->
+			
+			
+
+			
+
+				
+				
+
+			</div><!--section-->
+		</div><!--/.container-->
+    </section><!--/about-us-->
+	
+<?php include "footer.php";
+?>    
+
+    <script src="js/jquery.js"></script>
+    <script type="text/javascript">
+        $('.carousel').carousel()
+    </script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/wow.min.js"></script>
+</body>
+</html>
